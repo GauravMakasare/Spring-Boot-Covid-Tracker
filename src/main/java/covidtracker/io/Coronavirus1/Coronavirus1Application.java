bc0742dc0f2ac421f07b71b8b65a79f98994402a
GauravMakasare <gauravmakasare115@gmail.com>
2021-06-08 20:06:29 +0530

    refreshing
    Adding @EnableScheduling annotation to constantly refresh the page and get new and updated number of cases

--- src/main/java/covidtracker/io/Coronavirus1/Coronavirus1Application.java
+++ src/main/java/covidtracker/io/Coronavirus1/Coronavirus1Application.java
@@ -2,8 +2,10 @@ package covidtracker.io.Coronavirus1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Coronavirus1Application {
 
     public static void main(String[] args) {
--- src/main/java/covidtracker/io/Coronavirus1/services/CoronaVirusDataVirus.java
+++ src/main/java/covidtracker/io/Coronavirus1/services/CoronaVirusDataVirus.java
@@ -1,7 +1,9 @@
 package covidtracker.io.Coronavirus1.services;
 
+import covidtracker.io.Coronavirus1.models.LocationStats;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -11,6 +13,8 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 //makes it as a spring service
 @Service
@@ -18,10 +22,14 @@ public class CoronaVirusDataVirus {
 
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
+    private List<LocationStats> allStats= new ArrayList<>();
+
     @PostConstruct
+    @Scheduled(cron = "* * 1 * * *")  //cron has 6 parameters [ss,mm hh dd mm yy]/*this annotation is used to schedule the run of the method on regular intervals*/
+
     //tells the spring container to execute the following method
     public void fetchVirusData() throws IOException, InterruptedException {
-
+        List<LocationStats> newStats = new ArrayList<>();
         //creating a new Http Client
         HttpClient client = HttpClient.newHttpClient();
 
@@ -36,12 +44,19 @@ public class CoronaVirusDataVirus {
 
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
 
+        //go through each record and print them
+
         for (CSVRecord record : records){
 
-            String state = record.get("Province/State");
-            System.out.println(state);
+            LocationStats locationStat = new LocationStats();
+            locationStat.setState(record.get("Province/State"));
+            locationStat.setCountry(record.get("Country/Region"));
+            locationStat.setLatestTotalCases(Integer.parseInt(record.get(record.size() - 1)));
+            System.out.println(locationStat);
 
+            newStats.add(locationStat);
         }
+        this.allStats = newStats;
     }
 
 }
